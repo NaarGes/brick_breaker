cd6cc77488062460d439bdf734685dd6cef0a0ee
Narges Sadeghi <narges.sadeghi73@gmail.com>
2024-01-15 17:41:40 +0330

    Add ball to game world

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:brick_breaker/src/components/components.dart';
 import 'package:brick_breaker/src/config.dart';
@@ -15,7 +16,9 @@ class BrickBreaker extends FlameGame {
         );
 
   double get width => size.x;
+
   double get height => size.y;
+  final rand = math.Random();
 
   @override
   FutureOr<void> onLoad() {
@@ -23,5 +26,16 @@ class BrickBreaker extends FlameGame {
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+
+    world.add(
+      Ball(
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2).normalized()
+          ..scale(height / 4),
+        position: size / 2,
+        radius: ballRadius,
+      ),
+    );
+
+    debugMode = true;
   }
 }
