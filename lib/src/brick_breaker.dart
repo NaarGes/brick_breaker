0664f1db8294b029ed314d6cc04db0be32e86e48
Narges Sadeghi <narges.sadeghi73@gmail.com>
2024-01-27 11:54:42 +0330

    Add bat and handle its collision with ball

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
 import 'dart:math' as math;
+import 'dart:ui';
 
 import 'package:brick_breaker/src/components/components.dart';
 import 'package:brick_breaker/src/config.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class BrickBreaker extends FlameGame with HasCollisionDetection {
+class BrickBreaker extends FlameGame with HasCollisionDetection, KeyboardEvents {
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -36,6 +40,27 @@ class BrickBreaker extends FlameGame with HasCollisionDetection {
       ),
     );
 
+    world.add(
+      Bat(
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95),
+        size: Vector2(batWidth, batHeight),
+      ),
+    );
+
     debugMode = true;
+  }
+
+  @override
+  KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+
+    return KeyEventResult.handled;
   }
 }
--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -2,6 +2,7 @@ import 'package:brick_breaker/src/brick_breaker.dart';
 import 'package:brick_breaker/src/components/components.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
 
 class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<BrickBreaker> {
@@ -37,9 +38,11 @@ class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<Bri
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        velocity.y = -velocity.y;
-        // removeFromParent();
+        add(RemoveEffect(delay: 0.35));
       }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x = velocity.x + (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else {
       debugPrint('Collision with others');
     }
